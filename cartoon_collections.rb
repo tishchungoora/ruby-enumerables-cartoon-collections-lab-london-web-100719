188aebbe9a5cc90840fab612702a8fcf735f7020
Tish Chungoora <tishal82@gmail.com>
2019-09-19 12:59:39 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -9,12 +9,10 @@ def summon_captain_planet(pcalls)
 end
 
 def long_planeteer_calls(calls)
-  result = false
-  
   if calls.any? { |n| n.length > 4 } == true
     result = true
   else
-    result
+    result = false
   end
 end
 
